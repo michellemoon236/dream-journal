5dc6dd0dc13d2c0a7537dcbfa8a06ab03de723d0
Michelle Moon <michellemoon236@gmail.com>
2019-06-10 19:57:50 +0000

    add login form

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -27,8 +27,8 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(username: params[:email])
+    if @user && @user.authenticate(params[:email])
       session[:user_id] = @user.id
       redirect '/welcome'
     else
