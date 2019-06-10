7ec1acfdd837dcdb072f8a6bfdeff668bbbbacf1
Michelle Moon <michellemoon236@gmail.com>
2019-06-10 22:57:25 +0000

    update categories index.erb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
   get '/categories' do
     if logged_in?
       @categories = current_user.categories.uniq
-      erb :'dreams/index'
+      erb :'categories/index'
     else
       redirect '/login'
     end
