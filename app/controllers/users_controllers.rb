31a33eb32f66aad34b9f60b4c9a2ad6479fa1459
Michelle Moon <michellemoon236@gmail.com>
2019-06-10 19:36:09 +0000

    create signup routes

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
   
+  get '/signup' do
+    if logged_in?
+      redirect '/welcome'
+    else
+      erb :'users/signup'
+    end
+  end
+  
+  post '/signup' do
+    if params[:username] == "" || params[:email] =="" || params[:password] ==""
+      redirect "/signup"
+    else 
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/welcome'
+    end
+  end 
+  
+  get '/welcome' do 
+    
+  end
+  
 end
