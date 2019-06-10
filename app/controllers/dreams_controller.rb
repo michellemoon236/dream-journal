ecda1ac8a6a9e5468acacdc86f4905b180c46c35
Michelle Moon <michellemoon236@gmail.com>
2019-06-10 20:20:43 +0000

    create /dreams route

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,3 +1,13 @@
 class DreamsController < ApplicationController
   
+  get '/dreams' do
+    if logged_in?
+      @dreams = current_user.dreams
+      erb: 'dreams/index'
+    else
+      redirect '/login'
+    end
+  end
+  
+  
 end
