09e1df4f0e5ae4b388ecf47fbc610d5f06543a72
Michelle Moon <michellemoon236@gmail.com>
2019-06-10 22:55:24 +0000

    create categories route

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,12 @@
 class CategoriesController < ApplicationController
+
+  get '/categories' do
+    if logged_in?
+      @categories = current_user.categories.uniq
+      erb :'dreams/index'
+    else
+      redirect '/login'
+    end
+  end
   
 end
