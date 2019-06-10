7dd238b72667495994e6d2a8fcc3e1841bc06eb5
Michelle Moon <michellemoon236@gmail.com>
2019-06-10 22:58:44 +0000

    create categories show route in controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,5 +8,14 @@ class CategoriesController < ApplicationController
       redirect '/login'
     end
   end
+
+  get '/categories/:id' do
+    if logged_in?
+      @category = Category.find_by_id(params[:id])
+      erb :'categories/show'
+    else
+      redirect '/login'
+    end
+  end
   
 end
