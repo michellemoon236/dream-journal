527545a1f39cdaf7c429dee1b47623121d465a6f
Michelle Moon <michellemoon236@gmail.com>
2019-06-14 18:17:19 +0000

    add rack flash gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require './config/environment'
+required 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,6 +8,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "password_security"
+    use Rack::Flash
   end
 
   get "/" do
