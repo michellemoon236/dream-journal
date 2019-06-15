f67a5bc0424a0a23a2c9a6946b768b5c50e8cccb
Michelle Moon <michellemoon236@gmail.com>
2019-06-15 22:09:58 +0000

    update quotes with single quotes

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   post '/signup' do
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
       flash[:message] = "*All fields required*"
-      redirect "/signup"
+      redirect '/signup'
     else 
       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       session[:user_id] = @user.id
